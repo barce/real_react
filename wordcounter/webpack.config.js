d35a267b29d71871ff749f0be4a223867a585536
Jim Barcelona <jim.barcelona@onit.com>
2018-09-13 10:58:58 -0500

    p.39 of react for real

--- wordcounter/webpack.config.js
+++ wordcounter/webpack.config.js
@@ -1,3 +1,17 @@
+const path = require('path');
 module.exports = {
   entry: { app: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'app-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader']
+      }
+    ]
+  }
 };
