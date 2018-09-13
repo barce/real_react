60fe0b03d99463df7a0aa490d698fac161ad780d
Jim Barcelona <jim.barcelona@onit.com>
2018-09-13 13:14:46 -0500

    adding a test

--- wordcounter/src/__tests__/count_test.js
+++ wordcounter/src/__tests__/count_test.js
@@ -4,4 +4,7 @@ describe('the counting function', () => {
   it('counts the correct number of words', () => { 
     expect(countWords('One two three')).toBe(3);
   });
+  it('counts an empty string as zero words', () => {
+    expect(countWords('')).toBe(0);
+  });
 });
