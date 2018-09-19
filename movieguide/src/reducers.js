e13b34a6978d337e4ccb352b2dbc4b85d76bdbc7
Jim Barcelona <jim.barcelona@onit.com>
2018-09-19 09:36:08 -0500

    adding actions/reducers

---
+++ movieguide/src/reducers.js
@@ -0,0 +1,33 @@
+import { 
+  FILTER_CHANGED, 
+  MOVIES_LOADED, 
+  FAVORITED, 
+  UNFAVORITED
+} from './actions';
+
+export function movies(state = [], action) { 
+  switch (action.type) {
+    case MOVIES_LOADED: 
+      return action.movies;
+    default:
+      return state;
+  } 
+}
+
+export function loading(state = true, action) { 
+  switch (action.type) {
+    case MOVIES_LOADED: 
+      return false;
+    default:
+      return state;
+  } 
+}
+
+export function filter(state = false, action) { 
+  switch (action.type) {
+    case FILTER_CHANGED: 
+      return action.filter;
+    default:
+      return state;
+  } 
+}
